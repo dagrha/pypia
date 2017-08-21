de8eab387120a8a81ea3195b237d3515a99ae335
dagrha <dan@hallau.us>
2017-08-21 13:37:35 -0600

    updating version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'pypia',
     packages = ['pypia'],
-    version = '0.2.10',
+    version = '0.3.0',
     package_data = {'pypia': ['package_info.json']},
     author = 'Dan Hallau',
     author_email = 'pia@hallau.us',
